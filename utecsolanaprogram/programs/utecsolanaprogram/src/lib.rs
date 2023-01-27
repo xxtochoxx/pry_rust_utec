390c014a425ed8a264d2db86485772602b3ea1ff
Humberto Melendez <xxtochoxx@gmail.com>
2023-01-26 21:39:23 -0500

    Add files via upload

---
+++ utecsolanaprogram/programs/utecsolanaprogram/src/lib.rs
@@ -0,0 +1,250 @@
+/* 
+// ----------------------------------------- Programa Inicial --------------------------------------
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod utecsolanaprogram {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
+
+// ----------------------------------------- Programa Inicial --------------------------------------
+*/
+
+
+/*// ----------------------------------------- Programa 2 --------------------------------------
+
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod utecsolanaprogram {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        // Get a reference to the account.
+        let base_account = &mut ctx.accounts.base_account;
+        // Initialize total_gifs.
+        base_account.total_gifs = 0;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize <'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+// system_program permite que los programas 
+// de solana gestionen cuentas 
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
+
+// ----------------------------------------- Programa 2 -------------------------------------- 
+
+*/
+
+
+
+// ----------------------------------------- Programa 3 --------------------------------------
+
+use anchor_lang::prelude::*;
+
+//declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+declare_id!("Cz8gb1LckS8MEELA66Kns2jpL8NK6wv1smn1bd6arGBQ");
+
+
+#[program]
+pub mod utecsolanaprogram {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        // Get a reference to the account.
+        let base_account = &mut ctx.accounts.base_account;
+        // Initialize total_gifs.
+        base_account.total_gifs = 0;
+        Ok(())
+    }
+
+    // The function now accepts a gif_link param from the user. We also reference the user from the Context
+    pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> Result <()> {
+        let base_account = &mut ctx.accounts.base_account;
+        let user = &mut ctx.accounts.user;
+
+        // Build the struct.
+        let item = ItemStruct {
+            gif_link: gif_link.to_string(),
+            user_address: *user.to_account_info().key,
+        };
+            
+        // Add it to the gif_list vector.
+        base_account.gif_list.push(item);
+        base_account.total_gifs += 1;
+        Ok(())
+  }
+}
+
+#[derive(Accounts)]
+pub struct Initialize <'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+// Specify what data you want in the AddGif Context.
+// Add the signer who calls the AddGif method to the struct so that we can save it
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
+}
+
+// Create a custom struct for us to work with.
+// serialize y deserialize permiten convertir en bytes 
+// en la blockchain
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+    pub gif_link: String,
+    pub user_address: Pubkey, // la direccion del usuario que cargo el gif_link
+}
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+    // Attach a Vector of type ItemStruct to the account.
+    pub gif_list: Vec<ItemStruct>,
+}
+
+// ----------------------------------------- Programa 3 --------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod utecsolanaprogram {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        // Get a reference to the account.
+        let base_account = &mut ctx.accounts.base_account;
+        // Initialize total_gifs.
+        base_account.total_gifs = 0;
+        Ok(())
+    }
+
+    // The function now accepts a gif_link param from the user. We also reference the user from the Context
+    pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> Result <()> {
+        let base_account = &mut ctx.accounts.base_account;
+        let user = &mut ctx.accounts.user;
+
+        // Build the struct.
+        let item = ItemStruct {
+            gif_link: gif_link.to_string(),
+            user_address: *user.to_account_info().key,
+        };
+            
+        // Add it to the gif_list vector.
+        base_account.gif_list.push(item);
+        base_account.total_gifs += 1;
+        Ok(())
+  }
+}
+
+#[derive(Accounts)]
+pub struct Initialize <'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+
+// En solana la data esta en los accounts 
+// maxima tamaño es aprox 10mb 
+// el programa tambien se almacena en una cuenta 
+// esta es la diferencia respecto a solidity 
+
+
+// en la cuenta que creemos vamos a almacenar las urls
+// de las imagenes gifs
+
+// Specify what data you want in the AddGif Context.
+// Add the signer who calls the AddGif method to the struct so that we can save it
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
+}
+
+// Create a custom struct for us to work with.
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+    pub gif_link: String,
+    pub user_address: Pubkey,
+}
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+    // Attach a Vector of type ItemStruct to the account.
+    pub gif_list: Vec<ItemStruct>,
+}
+
+*/
